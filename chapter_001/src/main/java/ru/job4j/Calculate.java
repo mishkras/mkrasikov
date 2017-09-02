d65498e79142589e6dab2f531e967d108a43af22
Mikhail Krasikov <mk3470@yandex.ru>
2017-09-02 23:30:37 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,19 @@
 package ru.job4j;
 
+/**
+* Package for calculate task.
+*
+* @author Mikhail Krasikov (mailto:mishrkas@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
 public class Calculate {
+	 /**
+     * Main.
+     * @param args - args.
+     */
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+	
 }
