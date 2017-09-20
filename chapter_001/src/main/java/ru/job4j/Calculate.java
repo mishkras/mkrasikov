87bebfb521362687a6a81db8837d8a6e97f00567
Mikhail Krasikov <mk3470@yandex.ru>
2017-09-20 10:45:41 +0300

    task 4417

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -12,15 +12,15 @@ public class Calculate {
 	 * Main.
 	 * @param args - args.
 	 */
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
 	/**
 	 * Method echo.
 	 * @param name Your name.
 	 * @return Echo plus your name.
 	 */
 	public String echo(String name) {
-    return "Echo, echo, echo : " + name;
+		return "Echo, echo, echo : " + name;
 	}
 }
