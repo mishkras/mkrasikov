f01cd670cc2384b95d870ec5b87ec1ffa61d1987
Mikhail Krasikov <mk3470@yandex.ru>
2017-11-07 00:40:11 +0300

    task #188

--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-
 public class MaxTest {
     @Test
     public void whenFirstLessSecond() {
