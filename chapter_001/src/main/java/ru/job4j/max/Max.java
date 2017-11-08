81c1300585e77c1da875a3b0ba628758b6eaf143
Mikhail Krasikov <mk3470@yandex.ru>
2017-11-08 13:13:50 +0300

    task 189

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -1,8 +1,19 @@
 package ru.job4j.max;
 
+/**
+ * Max.
+ *
+ * @author Mikhail Krasikov (mailto:mishrkas@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 public class Max{
     public int max(int first, int second) {
         int maximum = first > second ? first : second;
         return maximum;
+    }
+
+    public int max(int first, int second, int third) {
+        return max(max(first, second), third);
     }
 }
--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -22,4 +22,11 @@ public class MaxTest {
         int result = maxim.max(1, 2);
         assertThat(result, is(2));
     }
+
+    @Test
+    public void whenFirstLessSecondLessTird() {
+        Max maxim = new Max();
+        int result = maxim.max(1, 2, 3);
+        assertThat(result, is(3));
+    }
 }
