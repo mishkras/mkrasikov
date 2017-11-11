8dbdddec40bb8c64a3d98a0d0dc9d1928442fd53
Mikhail Krasikov <mk3470@yandex.ru>
2017-11-12 00:18:21 +0300

    task #193, change circle for

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -3,12 +3,11 @@ package ru.job4j.loop;
 public class Factorial {
     public int calc(int n) {
         int factorial = 1;
+        if (n == 0) {
+            return 1;
+        }
         for (int i = 2; i <= n; i++) {
-            if (n == 0) {
-                factorial = 1;
-            } else {
-                factorial *= i;
-            }
+            factorial *= i;
         }
         return factorial;
     }
