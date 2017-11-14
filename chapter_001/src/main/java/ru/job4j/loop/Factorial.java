8ed35554bacbcbd1dafd61bf2d425d6d24833f48
Mikhail Krasikov <mk3470@yandex.ru>
2017-11-15 01:07:55 +0300

    [#193]

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -3,11 +3,10 @@ package ru.job4j.loop;
 public class Factorial {
     public int calc(int n) {
         int factorial = 1;
-        if (n == 0) {
-            return 1;
-        }
-        for (int i = 2; i <= n; i++) {
-            factorial *= i;
+        if (n > 1) {
+            for (int i = 2; i <= n; i++) {
+                factorial *= i;
+            }
         }
         return factorial;
     }
